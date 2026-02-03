100122f7f59ba1eb009a32e028d45f46624f8c6d
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2026-02-03 18:06:15 +0300

    Refactor WSGI entry point: remove virtual environment setup for cleaner code

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,13 +4,8 @@ Passenger WSGI Entry Point for cPanel
 import sys
 import os
 
-# Setup paths
-INTERP = '/home/whatsinu/virtualenv/africa.ugnews24.info/3.8/bin/python'
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
-
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, CURRENT_DIR)
+# Add paths
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # Import Flask app
 from app import app as application
